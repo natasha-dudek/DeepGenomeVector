fa144142edadbfae1ab9427cbebf907823cd80b6
Natasha Dudek <natasha@dudek.org>
2020-07-23 18:46:20 -0400

    add reshaping

--- memcache_client.py
+++ memcache_client.py
@@ -2,15 +2,35 @@ import socket
 import numpy as np
 from genome_embeddings import util
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(('localhost', 15555))
-idx = np.arange(4)
-#server.send(request.encode('utf8'))
-#response = server.recv(255).decode('utf8')
-util.send_msg(server, idx.tobytes())
-# server.send(idx.tobytes())
-#response = server.recv(255)
-response = util.recv_msg(server)
-print(len(response))
-out = np.frombuffer(response, np.float32)
-print(out)
+def _get_data(idx):
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.connect(('localhost', 15555))
+	#server.send(request.encode('utf8'))
+	#response = server.recv(255).decode('utf8')
+	util.send_msg(server, idx.tobytes())
+	# server.send(idx.tobytes())
+	#response = server.recv(255)
+	response = util.recv_msg(server)
+	print(len(response))
+	out = np.frombuffer(response, np.float32)
+	n_rows = len(idx)
+	n_cols = int(len(out) / n_rows)
+	out = np.reshape(out, (n_rows, n_cols))
+	return out
+
+
+def get_train_data(idx):
+	return _get_data(idx)
+	
+def get_test_data(idx):
+	# todo: figure out this number
+	offset = 100
+	idx = idx + offset
+	return _get_data(idx)
+	
+if __name__ == '__main__':
+	# example usage
+	idx = np.array([1, 4, 5])
+	print('train data', get_train_data(idx).shape)
+	print('test data', get_test_data(idx).shape)
+	
--- memcache_server.py
+++ memcache_server.py
@@ -6,10 +6,13 @@ import pandas as pd
 from genome_embeddings import util
 
 DATA_FP = '/Users/natasha/Desktop/mcgill_postdoc/ncbi_genomes/genome_embeddings/data/'
-# train_data = torch.load(DATA_FP+"corrupted_train_07-17-20.pt")
+
 
 # Keep data in numpy on the server side
+train_data = torch.load(DATA_FP+"corrupted_train_07-17-20.pt").numpy()
 test_data = torch.load(DATA_FP+"corrupted_test_07-17-20.pt").numpy()
+data = np.concatenate([train_data, test_data], 0)
+print(data.shape)
 #df, cluster_names = util.load_data(DATA_FP, "kegg")
 ## To make predictions on (ROC + AUC)
 #num_features = int(train_data.shape[1]/2)
